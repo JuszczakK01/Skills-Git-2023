0b7009160a10cae6c1f1303bbf71421bc884b0c8
JuszczakK01 <Juszczak.K01@elliotthudsoncollege.ac.uk>
2023-06-12 12:47:02 +0100

    End of task 2

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/Spikes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+	GameObject Hero;
+	// Use this for initialization
+	void Start () {
+		Hero = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnCollisionEnter2D(Collision2D other) {
+
+		Debug.Log ("SPIKED!");
+		Hero.SendMessage("resetPosition");
+	}
+}
+
