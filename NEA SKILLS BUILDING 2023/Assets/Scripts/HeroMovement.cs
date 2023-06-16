a6d8e994858cc7c861a10c9dc0b363f4ddf9c0e5
JuszczakK01 <Juszczak.K01@elliotthudsoncollege.ac.uk>
2023-06-16 10:28:09 +0100

    End of Task 4

--- NEA SKILLS BUILDING 2023/Assets/Scripts/HeroMovement.cs
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/HeroMovement.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HeroMovement : MonoBehaviour {
 	Rigidbody2D rb;
 	float speed;
+	public int lives;
+	public bool vulnerable;
 	// Use this for initialization
 	void Start () {
+		lives = 3;
 		speed = 8.1f;
+		vulnerable = false;
 		rb = GetComponent<Rigidbody2D> ();
 	}
 	
@@ -23,5 +28,31 @@ public class HeroMovement : MonoBehaviour {
 	void resetPosition(){
 		Debug.Log ("SPIKED RECEIVED!");
 		transform.SetPositionAndRotation( new Vector3 (-5.62f, 0.78f, 0), Quaternion.identity);
+		setLives ();
+	}
+	public void setLives(){
+		lives -= 1;
+		if (lives <= 0 || vulnerable == true) {
+			Debug.Log ("End of game");
+			SceneManager.LoadScene ("Lost");
+		}
+	}
+	private void OnCollisionEnter2D(Collision2D other){
+
+		if (other.gameObject.tag == "extraLife") {
+			lives += 1;
+			Destroy (other.gameObject);
+		} else if (other.gameObject.tag == "vulnerable") {
+			Destroy (other.gameObject);
+			vulnerable = true;
+			Debug.Log ("vulnerable = true!");
+			StartCoroutine ("VulnerableDeBuff");
+		}
+	}
+	IEnumerator VulnerableDeBuff()
+	{
+		yield return new WaitForSeconds(5f);
+		vulnerable = false;
+		Debug.Log ("vulnerable = false!");
 	}
 }
--- NEA SKILLS BUILDING 2023/Assets/Scripts/TextController.cs
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/TextController.cs
@@ -5,15 +5,22 @@ using TMPro;
 
 public class TextController : MonoBehaviour {
 	public TextMeshPro gemCounter;
+	public TextMeshPro livesCounter;
+	public TextMeshPro gameTimer;
+	int lives;
+	GameObject Hero;
 	GameObject [] totalGems;
 	// Use this for initialization
 	void Start () {
-		
+		Hero = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		totalGems = GameObject.FindGameObjectsWithTag ("redGem");
 		gemCounter.text = "Gems: " + totalGems.Length.ToString ();
+		lives = Hero.GetComponent<HeroMovement>().lives;
+		livesCounter.text = "Lives:" + lives;
+		gameTimer.text = "Time: " + Time.timeSinceLevelLoad.ToString ("0.00");
 	}
 }
