030233c5df5a911839a73fe773da4362670e879a
JuszczakK01 <Juszczak.K01@elliotthudsoncollege.ac.uk>
2023-06-19 13:17:55 +0100

    Task 8 - Slide 89
    Step 4

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public AudioClip coin;
+	private AudioSource source;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+
+}
