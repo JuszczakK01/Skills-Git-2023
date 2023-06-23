c779aeb7ff768a9e6eeaddcc17c1e940606455fa
JuszczakK01 <Juszczak.K01@elliotthudsoncollege.ac.uk>
2023-06-23 09:47:05 +0100

    End of task 8

--- NEA SKILLS BUILDING 2023/Assets/Scripts/SoundManager.cs
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/SoundManager.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 	public AudioClip coin;
+	public AudioClip bomb;
+	public AudioClip life;
 	private AudioSource source;
 	// Use this for initialization
 	void Start () {
-		
+			source = GetComponent<AudioSource> ();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	void Start () {
-		source = GetComponent<AudioSource> ();
+	private void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("bing!");
+		source.PlayOneShot (coin, 1);
 	}
-
+	public void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "extraLife") {
+			source.PlayOneShot (life, 1);
+		} else if (other.gameObject.tag == "vulnerable") {
+			source.PlayOneShot (bomb, 1);
+		}
+	}
+		
 }
