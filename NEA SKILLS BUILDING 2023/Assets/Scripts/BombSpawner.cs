9f559507f777f4fe6f58c81d276d74e8ad189291
JuszczakK01 <Juszczak.K01@elliotthudsoncollege.ac.uk>
2023-06-26 12:29:24 +0100

    End of Task 12

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawner : MonoBehaviour {
+	public GameObject bomb;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawner", 2, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Spawner (){
+		Instantiate (bomb, new Vector3 (26, 7, 0), Quaternion.identity);
+	}
+	void InvokeRepeating(){
+		Instantiate (bomb, new Vector3 (26.5289f, 6.874392f, 0), Quaternion.identity);
+	}
+}
---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, 10);
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-5, 8), ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnDestroy() {
+		Debug.Log ("A bomb was destroyed");
+	}
+}
